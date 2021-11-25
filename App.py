eca1260e6a8007b39ffc25bff252578856ffc16e
Ashiflko <ashifintegral@gmail.com>
2021-11-25 12:47:32 +0530

    complete

--- App.py
+++ App.py
@@ -25,11 +25,20 @@ elif category == websites[1]:
     news1 = scrapIndiatoday()
     for newsrow in news1:
         col1,col2=st.columns([4, 10])
-        col2.markdown(f' <h2> <a target="_blank" href="{newsrow["link"]}">{newsrow["heading"]}</a></h2> ', unsafe_allow_html=True)
+        col2.markdown(f' <h2> <a target="_blank" href="https://www.indiatoday.in/{newsrow["link"]}">{newsrow["heading"]}</a></h2> ', unsafe_allow_html=True)
         col1.markdown(f"![alt text]({newsrow['image']})")
+        #col1.markdown(f'<img src={newsrow["image"]} style="width: 50%" />', unsafe_allow_html=True)
         col2.write(newsrow['summary'])
         st.markdown("# ")
 
+elif category == websites[2]:
+    news1 = scrapNews18()
+    for newsrow in news1:
+        col1,col2=st.columns([4, 10])
+        col2.write(newsrow['heading'])
+        #col1.markdown(f"![alt text]({newsrow['image']})")
+        col2.write(newsrow['summary'])
+        #st.markdown("# ")
 
 
        
--- newsscrapper.py
+++ newsscrapper.py
@@ -55,5 +55,3 @@ def scrapIndiatoday():
     return newsdata
 
 
-
-  
